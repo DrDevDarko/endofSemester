72f017e8c19c32394ee4a16b18e07d58d88bad9d
Lexan <namaya438@gmail.com>
2024-11-13 09:47:15 -0500

    Initial Commit

---
+++ endofSemester/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  endofSemester
+//
+//  Created by Noe Amaya on 11/13/24.
+// testing git
+
+import UIKit
+
+class ViewController: UIViewController {
+var a = 0
+var b = 0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ endofSemesterTests/endofSemesterTests.swift
@@ -0,0 +1,17 @@
+//
+//  endofSemesterTests.swift
+//  endofSemesterTests
+//
+//  Created by Noe Amaya on 11/13/24.
+//
+
+import Testing
+@testable import endofSemester
+
+struct endofSemesterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
